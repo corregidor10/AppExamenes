1989e508124f9e02ba2b3848499c6408abf5d472
Daniel Corregidor Coronado <daniel.corregidor@tajamar365.com>
2016-05-11 17:25:51 +0200

    Add project files.

---
+++ AppExamenes/Features/Feature1/Feature1.feature
@@ -0,0 +1,13 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="babc6e80-e989-4c9a-975b-581fb23b9d88" description="SharePoint Add-in Feature" featureId="babc6e80-e989-4c9a-975b-581fb23b9d88" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="AppExamenes Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="14a080b1-0602-49ea-8ec1-d188fee6f053" />
+    <projectItemReference itemId="42a60308-a02f-426a-af82-f2f1ec08fec3" />
+    <projectItemReference itemId="9e6d7198-3a71-4332-bb8e-019b399e5472" />
+    <projectItemReference itemId="b73feecd-1f08-4c8b-b941-236348b21e75" />
+    <projectItemReference itemId="dc47319a-8aac-4420-9f9e-2d904fd90611" />
+    <projectItemReference itemId="7393b7d1-bd27-4812-948c-c6bd74a6a39b" />
+    <projectItemReference itemId="c858427d-9dfd-49df-8002-ea4e6cb13b08" />
+    <projectItemReference itemId="d29b0765-9263-402d-898c-6854e0ecc0b7" />
+  </projectItems>
+</feature>
---
+++ AppExamenes/Scripts/App.js
@@ -0,0 +1,73 @@
+﻿'use strict';
+
+function getAlumno() {
+
+    var alumno = $("#txtBus").val();
+
+    
+
+    $("#resultado").empty();
+    $("#resultado").append($("<img>", { src: "_layouts/images/gears_an.gif" }));
+
+    var requestUrl = _spPageContextInfo.webAbsoluteUrl + "/_api/lists/getByTitle('Alumno')/items?$filter=nombre eq '" + alumno+"'";
+
+   
+
+    $.ajax({
+        type: "GET",
+        url: requestUrl,
+        contentType: "application/json",
+        headers: {
+            "accept": "application/json;odata=verbose",
+        },
+        success: onReturnData,
+        error: function (xhr) {
+            alert(xhr.status);
+        }
+    });
+
+    }
+
+function onReturnData(data) {
+
+    $("#resultado").empty();
+    var oDataResult = data.d.results;
+
+    var tableHeader = "<thead>" +
+                       "<td>Nombre</td>" +
+                       "<td>Apellidos</td>" +
+                       "<td>DNI</td>" +
+                       "<td>Edad</td>" +
+                      "</thead>";
+
+    var table = $("<table>", { id: "alumnosTable" }).append($(tableHeader));
+
+    $.each(oDataResult,
+        function(i, item) {
+            var row = "<tr>" +"<td>" +item.nombre +"</td>" +"<td>" +item.apellidos +"</td>" +"<td>" +item.dni +"</td>" +"<td>" +item.edad +"</td></tr>";
+
+            table.append(row);
+
+        });
+
+    $("#resultado").append(table);
+
+
+}
+
+$(document).ready(function() {
+    $("#btnBus").click(getAlumno);
+});
+
+
+
+
+
+
+
+
+
+
+//var requestUrl = spPageContextInfo.webAbsoluteUrl + "_api/lists/getByTitle('Alumno')/items";
+
+//var requestUrl = "http://grupo33.azurewebsites.net/DataServiceExamenes.svc/Alumno?$filter=nombre eq'"+alumno+"'&$format=json";
